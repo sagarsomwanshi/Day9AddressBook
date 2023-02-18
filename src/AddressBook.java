d2d00617ce37979b8cc3994c86b9386a702a1b4b
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-18 08:50:33 +0530

    Added : Adds contact  information to address book

---
+++ src/AddressBook.java
@@ -0,0 +1,102 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class AddressBook {
+    String  name,lastName,address,city,state,email;
+    int zip;
+    long phone;
+
+    public AddressBook(String name, String lastName, String address, String city, String state, String email, int zip, long phone) {
+        this.name = name;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.email = email;
+        this.zip = zip;
+        this.phone = phone;
+    }
+
+    public AddressBook() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", email='" + email + '\'' +
+                ", zip=" + zip +
+                ", phone=" + phone +
+                '}';
+    }
+}
+
---
+++ src/AddressBookMain.java
@@ -0,0 +1,7 @@
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBook adr = new AddressBook("sagar","somwanshi","Society","Solapur","Maharashtra","sagarsomwanshi13@gmail.com",413004,8483820145l);
+        System.out.println(adr);
+    }
+
+}
